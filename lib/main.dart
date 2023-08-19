b76a011219ab1aac7499a875f722ab3251693ae4
MeDoRa0 <mohamed.hossam93x@gmail.com>
2023-08-19 03:14:52 +0300

    creat main file and spalshview file and  its body

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyMeds());
+}
+class MyMeds extends StatelessWidget {
+  const MyMeds({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
